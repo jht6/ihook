bddb6aa5ae3bc018eb62abd8d1912010b40233b2
Orange-J <juhaotian@foxmail.com>
2018-11-27 10:39:13 +0800

    [feat] getPkgJson() in jsEntry

--- scripts/run/index.js
+++ scripts/run/index.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('js entry running..');
+const path = require('path');
+const {
+    getPackageJsonDirPath,
+    readPackageJson
+} = require('../../common/utils');
+
+function getPkgJson() {
+    let ret;
+    try {
+        ret = readPackageJson(
+            path.join(getPackageJsonDirPath(), 'package.json')
+        );
+    } catch (e) {
+        ret = null;
+    }
+
+    return ret;
+}
+
+console.log(getPkgJson());
