49b55d106e4fc089d4e0b3925b26dcd564a8992b
Orange-J <juhaotian@foxmail.com>
2018-11-29 20:38:06 +0800

    [feat] commonExecutor

---
+++ scripts/run/executors/__tests__/commonExecutor.js
@@ -0,0 +1,4 @@
+// TODO: commonExecutor的单测
+test('commonExecutor', () => {
+    expect(true).toBe(true);
+});
--- scripts/run/executors/commonExecutor.js
+++ scripts/run/executors/commonExecutor.js
@@ -1,7 +1,24 @@
+const path = require('path');
+const execa = require('execa');
+
+/**
+ * Common task executor
+ * @param {Object} task shaped like { type: 'common', command: 'npm test' }
+ * @return {Number} exit code, if no error occurs, it's 0, else it's non-zero.
+ */
 module.exports = (task) => {
-    // TODO: 执行普通型任务，使用execa.shellSync()执行command即可
-    console.log('========= commonExecutor start ==========');
-    console.log(task);
-    console.log('========= commonExecutor   end ==========');
-    return 0;
+    const scriptPath = process.argv[1];
+    const cwd = path.resolve(scriptPath.split('node_modules')[0]);
+
+    let exitCode = 0;
+    try {
+        execa.shellSync(task.command, {
+            cwd,
+            stdio: 'inherit'
+        });
+    } catch (e) {
+        exitCode = e.code || 1;
+    }
+
+    return exitCode;
 };
