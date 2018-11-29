1c2c126f755d55848d823505fc5238de1a527c2b
cloudtian <cloud_tian@foxmail.com>
2018-11-29 22:15:41 +0800

    [test] add unit testing for "commonExecutor"

--- scripts/run/executors/__tests__/commonExecutor.js
+++ scripts/run/executors/__tests__/commonExecutor.js
@@ -1,4 +1,20 @@
-// TODO: commonExecutor的单测
-test('commonExecutor', () => {
-    expect(true).toBe(true);
+const { common } = require('..');
+
+test('return 0 if the task command executed successfully', () => {
+    let result = common({
+        command: 'ls'
+    });
+    expect(result).toBe(0);
+});
+
+test('return none-zero if a none-zero exit character is assigned to exit command', () => {
+    let result = common({
+        command: 'exit 8'
+    });
+    expect(result).not.toBe(0);
+});
+
+test('return none-zero if the task command is empty', () => {
+    let result = common();
+    expect(result).not.toBe(0);
 });
