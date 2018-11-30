4b9de59361a41a0837107a688c1998e6e19bdd1a
Orange-J <juhaotian@foxmail.com>
2018-11-30 09:54:58 +0800

    [test] add a unit test case for "commonExecutor"

--- scripts/run/executors/__tests__/commonExecutor.js
+++ scripts/run/executors/__tests__/commonExecutor.js
@@ -1,20 +1,27 @@
-const { common } = require('..');
+const commonExecutor = require('..').common;
 
 test('return 0 if the task command executed successfully', () => {
-    let result = common({
+    let result = commonExecutor({
         command: 'ls'
     });
     expect(result).toBe(0);
 });
 
-test('return none-zero if a none-zero exit character is assigned to exit command', () => {
-    let result = common({
+test('return non-zero value if a non-zero exit character is assigned to exit command', () => {
+    let result = commonExecutor({
         command: 'exit 8'
     });
     expect(result).not.toBe(0);
 });
 
-test('return none-zero if the task command is empty', () => {
-    let result = common();
+test('return non-zero value if the task command is empty', () => {
+    let result = commonExecutor();
+    expect(result).not.toBe(0);
+});
+
+test('return non-zero value if the task command is invalid shell code', () => {
+    let result = commonExecutor({
+        command: 'nosuchcommand'
+    });
     expect(result).not.toBe(0);
 });
