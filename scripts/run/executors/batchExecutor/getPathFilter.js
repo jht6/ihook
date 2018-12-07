0d41517018ce7e549432751d4a4036ab46de2fb9
Orange-J <juhaotian@foxmail.com>
2018-12-07 14:48:29 +0800

    [docs] adjust TODO in "getPathFilter.js"

--- scripts/run/executors/batchExecutor/getPathFilter.js
+++ scripts/run/executors/batchExecutor/getPathFilter.js
@@ -3,14 +3,18 @@
 /**
  * Create a filter function according to task.filter config.
  * @param {Function|Array} filterConfig
- *  a filter function or an array contains file paths shaped like ['.eslintignore']
+ *  a filter function or an object shaped like:
+ *  {
+ *      extensions: ['.js'],
+ *      ignoreRuleFiles: ['.eslintignore']
+ *  }
  */
 module.exports = filterConfig => {
     let ret;
     if (typeof filterConfig === 'function') {
         ret = filterConfig;
-    } else if (Array.isArray(filterConfig)) {
-        // TODO: 通过['.eslintignore']这样的数组
+    } else if (typeof filterConfig === 'object' && filterConfig !== null) {
+        // TODO: 通过extensions和ignoreRuleFiles创建一个过滤函数
     } else {
         ret = null;
     }
