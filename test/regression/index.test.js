2a84aac0a562bc52332939baa8bb2f7a1616ec81
Orange-J <juhaotian@foxmail.com>
2018-12-14 11:55:51 +0800

    [test][refactor] adjust variable name and remove useless code

--- test/regression/index.test.js
+++ test/regression/index.test.js
@@ -10,18 +10,15 @@ const {
 } = require('../../common/const')();
 const beforeTest = require('./beforeTest');
 
-const PCE_ROOT_DIR = process.cwd(); // This module's git reposition root dir path.
-const TESTING_DIR_NAME = 'sandbox';
-// const TESTING_DIR_PATH = path.join(PCE_ROOT_DIR, TESTING_DIR_NAME);
-// const PACKAGE_JSON_PATH = path.join(TESTING_DIR_PATH, 'package.json');
+const TEST_DIR_NAME = 'sandbox';
 
 beforeAll(() => {
-    beforeTest(TESTING_DIR_NAME);
+    beforeTest(TEST_DIR_NAME);
 });
 
 afterAll(() => {
     if (typeof __SAVE_SANDBOX__ === 'undefined') {
-        execSync(`rm -rf ${TESTING_DIR_NAME}`);
+        execSync(`rm -rf ${TEST_DIR_NAME}`);
     }
 });
 
@@ -32,7 +29,7 @@ describe('regression - install.js', () => {
     beforeAll(() => {
         try {
             execSync([
-                `cd ${TESTING_DIR_NAME}`,
+                `cd ${TEST_DIR_NAME}`,
                 `node ./node_modules/ihook/scripts/install/index.js`
             ].join(` && `));
         } catch (e) {
@@ -47,13 +44,13 @@ describe('regression - install.js', () => {
     // TODO: other hooks except "pre-commit"
     test('install "pre-commit" hook file in .git/hooks', () => {
         expect(
-            fs.existsSync(`./${TESTING_DIR_NAME}/.git/hooks/pre-commit`)
+            fs.existsSync(`./${TEST_DIR_NAME}/.git/hooks/pre-commit`)
         ).toBe(true);
     });
 
     test(`copy "${CONFIG_FILE_NAME}" to "package.json"\'s dir`, () => {
         expect(
-            fs.existsSync(`./${TESTING_DIR_NAME}/${CONFIG_FILE_NAME}`)
+            fs.existsSync(`./${TEST_DIR_NAME}/${CONFIG_FILE_NAME}`)
         ).toBe(true);
     });
 });
@@ -66,7 +63,7 @@ describe('regression - scripts/run/index.js(common hook)', () => {
     beforeAll(() => {
         try {
             execSync([
-                `cd ${TESTING_DIR_NAME}`,
+                `cd ${TEST_DIR_NAME}`,
                 `echo foo >> ${commited}`,
                 `git add ${commited}`,
                 `git commit -m test`
