594104d144788e155a1e41223fda7656b31d7172
Orange-J <juhaotian@foxmail.com>
2018-12-20 23:52:09 +0800

    [fix] fix #8: getFilePathList() should filter paths correctly

--- common/utils/__tests__/getFilePathList.js
+++ common/utils/__tests__/getFilePathList.js
@@ -8,31 +8,22 @@ describe('can correctly execute the first filtering', () => {
         skipFilterNotExist: true
     };
 
-    test('can correctly filter empty string', () => {
+    test('can correctly filter paths', () => {
         const gitStatus = [
-            `MM a.js\n`,
-            `A  b.js\n`,
-            `?? c.js\n`,
-            ` A d.js\n`,
-            ` D e.js\n`
+            `M  m.js\n`,
+            `A  a.js\n`,
+            `R  ro.js -> r.js\n`,
+            `?? un.js\n`,
+            ` M ea.js\n`,
+            ` A ea.js\n`,
+            ` D ed.js\n`
         ].join('');
 
         const list = getFilePathList(gitStatus, testFlag);
         expect(list.length > 0).toBe(true);
-
-        const emptyList = list.filter(item => !item);
-        expect(emptyList.length).toBe(0);
-    });
-
-    test('can correctly filter string starts with "??"', () => {
-        let gitStatus = `?? a.js\n?? b.js\n`;
-        let list = getFilePathList(gitStatus, testFlag);
-        expect(list.length).toBe(0);
-
-        gitStatus = `M  a.js\n?? b.js\n`;
-        list = getFilePathList(gitStatus, testFlag);
-        expect(list.length).toBe(1);
-        expect(list[0]).toBe('M  a.js');
+        expect(list[0]).toBe('m.js');
+        expect(list[1]).toBe('a.js');
+        expect(list[2]).toBe('r.js');
     });
 });
 
@@ -43,9 +34,10 @@ test('can correctly map path to absolute path', () => {
         gitRoot,
         skipFilterNotExist: true
     };
-    const gitStatus = 'M  a.js\n A b.js\n';
+    const gitStatus = 'M  a.js\nA  b.js\nR  ro.js -> r.js\n';
     const list = getFilePathList(gitStatus, testFlag);
-    expect(list.length).toBe(2);
+    expect(list.length).toBe(3);
     expect(list[0]).toBe(path.join(gitRoot, `a.js`));
     expect(list[1]).toBe(path.join(gitRoot, `b.js`));
+    expect(list[2]).toBe(path.join(gitRoot, `r.js`));
 });
--- common/utils/getFilePathList.js
+++ common/utils/getFilePathList.js
@@ -6,6 +6,16 @@ const getGitRootDirPath = require('./getGitRootDirPath');
 /**
  * Get list of absolute file paths from string of git status, only retaining
  * paths of files which are being committed.
+ *
+ * According to git docs on https://git-scm.com/docs/git-status, we should parse
+ * the paths with "M"/"A"/"R" status on X position, for example("*" at Y position means arbitrary status):
+ * M* PATH1 // a file which has been committed already is modified and added into index now
+ * A* PATH2 // a new file which has never been committed is added into index now
+ * R* ORIG_PATH -> PATH // a file which has been committed already is renamed or moved to another position
+ *
+ * According to https://stackoverflow.com/questions/27198414/git-status-git-diff-c-not-detecting-file-copy-in-index,
+ * we don't need to pay attension to the "C" status code.
+ *
  * @param {String} gitStatusStr output of running "git status --porcelain"
  * @param {Object} testFlag just for testing.
  * @return {Array} list of paths
@@ -16,13 +26,24 @@ function getFilePathList(gitStatusStr, testFlag) {
     const gitRoot = testFlag.isTesting && testFlag.gitRoot ?
         testFlag.gitRoot : getGitRootDirPath(process.cwd());
 
-    let pathList = gitStatusStr.split('\n')
-        // Exclude strings which is empty or starts with "??"(Untraced paths)
-        .filter(item => !!item && !/^\?\?/.test(item));
+    let pathList = gitStatusStr.replace(/\r/g, '').split('\n')
+        .filter(item => {
+            // only remain items whose first char is 'M' or 'A' or 'R'
+            return !!item && /^[MAR]$/.test(item.charAt(0))
+        })
+        .map(item => {
+            if (item.charAt(0) === 'R') {
+                // take out "PATH" from "R* ORIG_PATH -> PATH"
+                return /\s\->\s(.*)$/.exec(item)[1];
+            } else {
+                // take out "PATH" from "XY PATH"
+                return item.substring(startIndex);
+            }
+        });
 
     // Transform to absolute path
     if (!(testFlag.isTesting && testFlag.skipMapToAbsPath)) {
-        pathList = pathList.map(item => path.join(gitRoot ,item.substring(startIndex)));
+        pathList = pathList.map(item => path.join(gitRoot ,item));
     }
 
     // Confirm the path exists
